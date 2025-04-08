f41ace6dd8ef65d19eb56b988489a13f732ce8d1
tang tang <1524533474@qq.com>
2025-04-08 21:49:43 +0800

    继续备份

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private SpriteRenderer Renderer;
+    public float speed = 50f;
+    public float jumpspeed = 50f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb=GetComponent<Rigidbody2D>();
+        Renderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float h=Input.GetAxis("Horizontal");
+
+
+        if(h>0)
+        {
+            Renderer.flipX = false;
+        }
+        else if (h < 0)
+        {
+            Renderer.flipX = true;
+        }
+
+        rb.velocity = new Vector2(h*speed, rb .velocity.y);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpspeed);
+        }
     }
 }
--- Assets/NewBehaviourScript1.cs
+++ Assets/NewBehaviourScript1.cs
@@ -4,25 +4,40 @@ using UnityEngine;
 
 public class NewBehaviourScript1 : MonoBehaviour
 {
-    public Rect Rect = new Rect(10, 10, 100, 100);
+    public Rect rect1 = new Rect(10, 10, 100, 100);
+    public Rect rect2 = new Rect(10, 60, 100, 100);
+    public Rect rect3 = new Rect(10, 120, 100, 100);
     public bool duibu = true;
     public GUIStyle GUIStyle;
     public GUIContent a;
+
+    public int nowselindex = 1;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     private void OnGUI()
     {
 
-        duibu=GUI.Toggle(Rect, duibu, "hahahsudhbsdvsd",GUIStyle);
 
+        if (GUI.Toggle(rect1, nowselindex == 1, "选项A"))
+        {
+            nowselindex = 1;
+        }
+        if (GUI.Toggle(rect2, nowselindex == 2, "选项B"))
+        {
+            nowselindex = 2;
+        }
+        if (GUI.Toggle(rect3, nowselindex == 3, "选项C"))
+        {
+            nowselindex = 3;
+        }
     }
 }
