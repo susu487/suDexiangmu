2cae0163d21fc15369a22adf76fa17e5135380e9
susu487 <2248171651@qq.com>
2025-04-09 19:57:41 +0800

    点击跳转

--- Assets/UI/a1.cs
+++ Assets/UI/a1.cs
@@ -6,6 +6,24 @@ using UnityEngine.UI;
 
 public class a1 : MonoBehaviour
 {
+    private static a1 instance;
+    public static void showme()
+    {
+        if (instance != null)
+        {
+            //物体不失活
+            instance.gameObject.SetActive(true);
+        }
+    }
+    public static void hideme()
+    {
+        if (instance != null)
+        {
+            //物体失活
+            instance.gameObject.SetActive(false);
+        }
+    }
+    public GUIStyle style;
     public Texture tex;
     public Rect rect;
     public Rect rect1;
@@ -13,19 +31,30 @@ public class a1 : MonoBehaviour
     public Rect rect3;
     public GUIContent content;
     public GUIStyle buttonstyle;
-    //public string text;
-    // Start is called before the first frame update
+    
+    private void Awake()
+    {
+        instance = this;
+    }
     public void OnGUI()
     {
+       
         //标题
-        //GUI.Label(rect,"苏菲大女王", style);
+        GUI.Label(rect,"苏菲大女王", style);
         //开始，结束，设置
         if (GUI.Button(rect1, "开始游戏", buttonstyle))
         {
             SceneManager.LoadScene("GameScene");
         }
         GUI.Button(rect2, "退出游戏", buttonstyle);
-        GUI.Button(rect3, "设置游戏", buttonstyle);
+        if(GUI.Button(rect3, "设置游戏", buttonstyle))
+        {
+            
+            a3.showme();
+            Debug.Log("显示设置界面");
+            hideme();
+            Debug.Log("隐藏开始界面");
+        }
         
     }
 }
--- Assets/UI/a3.cs
+++ Assets/UI/a3.cs
@@ -5,18 +5,41 @@ using UnityEngine;
 
 public class a3 : MonoBehaviour
 {
+    private static a3 instance;
+    public static void showme()
+    {
+        if (instance != null)
+        {
+            //物体不失活
+            instance.gameObject.SetActive(true);
+        }
+    }
+    public static void hideme()
+    {
+        if (instance != null)
+        {
+            //物体失活
+            instance.gameObject.SetActive(false);
+        }
+    }
     private Boolean issel1;
     private Boolean issel2;
     public Rect rect1;
     public Rect rect2;
     public Rect rect3;
+    private void Awake()
+    {
+        instance = this;
+        hideme();
+    }
     private void OnGUI()
     {
         issel1 = GUI.Toggle(rect1, issel1, "音乐开关");
         issel2 = GUI.Toggle(rect2, issel2, "音效开关");
         if (GUI.Button(rect3, "关闭"))
         {
-
+            hideme();
+            a1.showme();
         }
     }
 }
